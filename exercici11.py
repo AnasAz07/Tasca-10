8b08621e910dc59bdc70077891d9670c85027558
AnasAz07 <aazaroual20745@iesjoanramis.org>
2023-10-24 14:02:02 +0200

    ex 11

---
+++ exercici11.py
@@ -0,0 +1,16 @@
+def menu_principal():
+    print("""
+           Menú Principal:
+          1. Calculadora de Nombres Enters
+          2. Calculadora de Nombres Reals
+          3. Sortir
+          """)
+    x = int(input("Elegeix una opció"))
+    if x>0 and x<4
+        return x
+    else:
+        return 0
+
+#Programa Principal
+a = menu_principal
+print(a)
