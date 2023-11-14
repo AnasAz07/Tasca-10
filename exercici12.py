0d57a2d73a859855d855fd75a9d7750cdd9e8b01
AnasAz07 <aazaroual20745@iesjoanramis.org>
2023-11-14 14:04:52 +0100

    ex12 començat

--- exercici12.py
+++ exercici12.py
@@ -76,6 +76,47 @@ def calculadora_reals():
             case 5: # Sortir
                 print("Ara tornaràs a la calculadora inicial \n\n")
                 op = -1
+def canvi_base():
+    print("Hem passat per canvi de base")
+    op = 1
+    while op>0:
+        print("""
+              Menú canvi de base
+              1. Donat un binari ho passem a tota la resta
+              2. Donat un octal ho passem a tota la resta
+              3. Donat un decimal ho passem a tota la resta
+              4. Donat un hexadecimal ho passem a tota la resta
+              5. Sortir
+        """)
+        op = int(input("Elegeix una opció: "))
+        match op:
+            case 1: # Binari to
+                b = input("Introdueix el número binari: ")
+                o = bintooct(b)
+                d = bintodec(b)
+                h = bintohex(b)
+                print("El número binari {} és: \n oct -> {} \n dec -> {} \n hex -> {}".format(b,o,d,h))
+            case 2: # Octal to
+                o = input("Introdueix el nombre octal: ")
+                b = octtobin(o)
+                d = octtodec(o)
+                h = octtohex(o)
+                print("El número binari {} és: \n bin -> {} \n dec -> {} \n hex -> {}".format(o,b,d,h))
+            case 3: # Decimal to
+                d = input("Introdueix el nombre decimal: ")
+                b = dectobin(d)
+                o = dectooct(d)
+                h = dectohex(d)
+                print("El número binari {} és: \n bin -> {} \n oct -> {} \n hex -> {}".format(d,b,o,h))
+            case 4: # Hexadecimal to
+                h = input("Introdueix el nombre hexadecimal: ")
+                b = hextobin(h)
+                o = hextooct(h)
+                d = hextodec(h)
+                print("El número binari {} és: \n bin -> {} \n oct -> {} \n dec -> {}".format(h,b,o,d))
+            case 5: # Sortir
+                print("Ara tornaràs al menu principal de canvi de base")
+                op = -1
 
 #Programa Principal
 
@@ -88,6 +129,8 @@ while a>0:
         case 2:
             calculadora_reals()
         case 3:
+            canvi_base()
+        case 4:
             a = -1
         case other:
             print("Opció no vàlida")
