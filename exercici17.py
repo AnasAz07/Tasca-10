e058e1bce908eaac938bcc31c685c7b256401ea2
AnasAz07 <aazaroual20745@iesjoanramis.org>
2023-11-24 09:50:47 +0100

    ex 17 completat

--- exercici17.py
+++ exercici17.py
@@ -1,4 +1,27 @@
-def sumer_llista():
-    
+def llegir_llista():
+    a = 'l'
+    l = []
+    while a!= '.':
+        a = input("Introduce la lista, para terminarla mete un . : ")
+        if a!= '.':
+            l.append(int(a))
+        else:
+            return l
+
+def sumar_llista(l):
+    s = 0
+    for e in l:
+        s += e
+    print("El resultat de la suma és {}".format(s))
+
+def multiplicar_llista(l):
+    m = 1
+    for e in l:
+        m *= e
+    print("El resultat de la multiplicació és {}".format(m))
+
+
 #Programa Principal
-a = list(input("Introdueix la llista que vols sumar els seus valors: "))
+l = llegir_llista()
+sumar_llista(l)
+multiplicar_llista(l)
