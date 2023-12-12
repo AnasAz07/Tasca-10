bf97cae0252b8a473703ff4ba45f02f6380295bf
AnasAz07 <aazaroual20745@iesjoanramis.org>
2023-12-12 13:27:27 +0100

    modificació ex 46

--- exercici46.py
+++ exercici46.py
@@ -1,10 +1,13 @@
 def esta_ordenada(a):
-    b = a.copy()
-    a.sort()
-    if a == b:
-        print("La llista {} està ordenada {}".format(a, b))
+    ascendente = sorted(a) == a
+    descendente = sorted(a, reverse = True) == a
+
+    if ascendente:
+        print("La llista {} està ordenada ascendentment".format(a))
+    elif descendente:
+        print("La llista {} està ordenada descendentment".format(a))
     else:
-        print("La llista {} no està ordenada {}".format(a, b))
+        print("La llista {} no està ordenada".format(a))
 
 def llegir_llista():
     a = []
